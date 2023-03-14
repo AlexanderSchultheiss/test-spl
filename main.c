a1e7168c9502b22d9ef03f64fe9a014e055b8d94
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-14 09:03:35 +0100

    removed variablity

--- main.c
+++ main.c
@@ -7,18 +7,6 @@
 int main(void) {
    printf("Here is the message: %s\n", MESSAGE);
 
-   #if 0
-    fprintf (stderr, "computed %d,%d,%d (%d) provided %d,%d,%d (%d)\n",
-	     m, n, r, S3_CLOCK(m,n,r),
-	     t->dac_m, t->dac_n, t->dac_r,
-	     S3_CLOCK(t->dac_m, t->dac_n, t->dac_r));
-#else
-fprintf(something)
-#if 1
-do bal
-   bla bla
-   blu
-#endif
-#endif
+
 hello
 }
