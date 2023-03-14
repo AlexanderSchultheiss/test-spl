cd5b3dfebc4cbb208834e0592d6141a707b8cdc8
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-14 09:04:41 +0100

    multiline if statements

--- main.c
+++ main.c
@@ -7,6 +7,17 @@
 int main(void) {
    printf("Here is the message: %s\n", MESSAGE);
 
+#if (!defined(HAVE_GETHOSTBYNAME_R) || !defined(HAVE_GETHOSTBYADDR_R) || \
+           !defined(HAVE_GETPWUID_R)      || !defined(HAVE_GETGRGID_R)) && \
+          (!defined(HAVE_MTSAFE_GETHOSTBYNAME) || !defined(HAVE_MTSAFE_GETHOSTBYADDR))
+          blabla
+          blabla
+#endif /* (!defined(HAVE_GETHOSTBYNAME_R) || !defined(HAVE_GETHOSTBYADDR_R) || \
+           !defined(HAVE_GETPWUID_R)      || !defined(HAVE_GETGRGID_R)) && \
+          (!defined(HAVE_MTSAFE_GETHOSTBYNAME) || !defined(HAVE_MTSAFE_GETHOSTBYADDR)) */
 
+#if (!defined(HAVE_GETHOSTBYNAME_R) || !defined(HAVE_GETHOSTBYADDR_R)) && \
+    (!defined(HAVE_MTSAFE_GETHOSTBYNAME) || !defined(HAVE_MTSAFE_GETHOSTBYADDR))
 hello
+#endif
 }
