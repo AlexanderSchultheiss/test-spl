31194a26db7c216f4f34948242bcf85b9cd63532
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-21 08:40:43 +0100

    nested if0 with else

---
+++ inner_false.c
@@ -0,0 +1,19 @@
+Hello
+Hello
+Hello
+#if defined HELLO
+something
+something
+#if 0
+nothing
+nothing
+#else
+always true
+always true
+#endif
+more something
+more something
+#endif
+World
+World
+World
